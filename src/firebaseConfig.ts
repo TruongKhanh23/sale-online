bbee66f4536267a931f3beb1079af78eaf2463b5
Truong Khanh <truongnguyenkhanh230800@gmail.com>
2024-10-27 21:07:03 +0700

    SOW-011: Setup environment DEV, UAT, PRD, Preview, Localhost

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,26 +1,26 @@
+// firebaseConfig.ts
+
 import { initializeApp } from 'firebase/app'
-// import { getAnalytics } from 'firebase/analytics';
-import { getFirestore } from 'firebase/firestore'
+import urlHostToConfigsMap from './urlHostToConfigsMap'
 
-// Cấu hình Firebase từ biến môi trường
-console.log(
-  'VITE_FIREBASE_AUTH_DOMAIN',
-  import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-)
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+type FirebaseConfig = {
+  apiKey: string
+  authDomain: string
+  projectId: string
+  storageBucket: string
+  messagingSenderId: string
+  appId: string
+  measurementId: string
 }
+
+const currentDomain: string = window.location.hostname
+
+// Lấy cấu hình từ file đã tạo
+const firebaseConfig: FirebaseConfig =
+  urlHostToConfigsMap[currentDomain] || urlHostToConfigsMap['localhost'] // Mặc định cho localhost nếu không khớp
 
 // Khởi tạo Firebase
 const app = initializeApp(firebaseConfig)
-// const analytics = getAnalytics(app); // Optional, nếu bạn không cần analytics thì có thể bỏ qua
-const db = getFirestore(app) // Khởi tạo Firestore
 
-export { app, db } // Xuất app và db
+// Xuất app và config
+export { app, firebaseConfig }
